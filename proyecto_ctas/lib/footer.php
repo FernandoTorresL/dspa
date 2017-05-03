d486b515782a39c7a74e44c0b3ea1c01d5fbd6cb
Fernando Torres <fertorresmx@gmail.com>
2017-05-02 19:41:42 -0500

    Se borró carpeta proyecto_ctas

--- proyecto_ctas/lib/footer.php
+++
@@ -1,8 +0,0 @@
-    <footer class="footer background">
-      <div class="contenedor2">
-            <a href="mailto:fernando.torresl@imss.gob.mx">Contactar Soporte Técnico</a>Derechos Reservados/Copyright &copy;2017 Enterprises ATM, Inc.<a href="http://www.linkedin.com/in/fertorresmx" target="_blank">Fernando Torres</a><!-- </p> -->
-      </div>
-    </footer>
-
-  </body>
-</html>
