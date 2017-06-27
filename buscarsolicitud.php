bf6420edd0e2b144907cc0156c895560fcf56c17
Fernando Torres <fertorresmx@gmail.com>
2017-06-27 11:09:15 -0500

    Mejoras en información que se visualiza en buscarsolicitud

--- buscarsolicitud.php
+++ buscarsolicitud.php
@@ -33,6 +33,7 @@
       exit();
     }
 
+
     $dbc = mysqli_connect( DB_HOST, DB_USER, DB_PASSWORD, DB_NAME );
 
     $query = "SELECT id_user 
@@ -84,33 +85,63 @@
       }
 
       // Obtener todas las solicitudes capturadas al momento para el penúltimo lote modificado
-      $query = "SELECT 
-                  ctas_solicitudes.id_solicitud, ctas_solicitudes.id_valija, ctas_valijas.num_oficio_ca, ctas_valijas.fecha_recepcion_ca, 
-                  ctas_solicitudes.fecha_captura_ca, ctas_solicitudes.fecha_solicitud_del, ctas_solicitudes.fecha_modificacion,
-                  ctas_lotes.lote_anio AS num_lote_anio, 
-                  ctas_solicitudes.delegacion AS num_del, dspa_delegaciones.descripcion AS delegacion_descripcion, 
-                  ctas_valijas.delegacion AS num_del_val, 
-                  ctas_solicitudes.subdelegacion AS num_subdel, dspa_subdelegaciones.descripcion AS subdelegacion_descripcion, 
-                  ctas_solicitudes.nombre, ctas_solicitudes.primer_apellido, ctas_solicitudes.segundo_apellido, 
-                  ctas_solicitudes.matricula, ctas_solicitudes.curp, ctas_solicitudes.curp_correcta, ctas_solicitudes.cargo, ctas_solicitudes.usuario, 
-                  ctas_movimientos.descripcion AS movimiento_descripcion, 
-                  grupos1.descripcion AS grupo_actual, grupos2.descripcion AS grupo_nuevo, 
-                  ctas_solicitudes.comentario, ctas_causasrechazo.id_causarechazo, ctas_causasrechazo.descripcion AS causa_rechazo, ctas_solicitudes.archivo,
-                  CONCAT(dspa_usuarios.nombre, ' ', dspa_usuarios.primer_apellido) AS creada_por,
-                  ctas_valijas.archivo AS archivovalija
-                FROM ctas_solicitudes, ctas_valijas, ctas_lotes, dspa_delegaciones, dspa_subdelegaciones, ctas_movimientos, ctas_grupos grupos1, ctas_grupos grupos2, dspa_usuarios, ctas_causasrechazo
-                WHERE ctas_solicitudes.id_lote       = ctas_lotes.id_lote
-                AND   ctas_solicitudes.id_valija     = ctas_valijas.id_valija
-                AND   ctas_solicitudes.delegacion    = dspa_subdelegaciones.delegacion
-                AND   ctas_solicitudes.subdelegacion = dspa_subdelegaciones.subdelegacion
-                AND   ctas_solicitudes.delegacion    = dspa_delegaciones.delegacion
-                AND   ctas_solicitudes.id_movimiento = ctas_movimientos.id_movimiento
-                AND   ctas_solicitudes.id_grupo_actual= grupos1.id_grupo
-                AND   ctas_solicitudes.id_grupo_nuevo= grupos2.id_grupo
-                AND   ctas_solicitudes.id_user = dspa_usuarios.id_user
-                AND   ctas_solicitudes.id_causarechazo = ctas_causasrechazo.id_causarechazo
-                AND   ctas_solicitudes.usuario LIKE '%" . $usuario . "%' 
-                ORDER BY ctas_solicitudes.fecha_captura_ca DESC, ctas_solicitudes.id_solicitud ASC, ctas_solicitudes.fecha_modificacion DESC";
+      $query = 'SELECT  
+                  S.id_solicitud, S.id_valija, V.num_oficio_ca, V.fecha_recepcion_ca, 
+                  S.fecha_captura_ca, S.fecha_solicitud_del, S.fecha_modificacion,
+                  L.lote_anio AS num_lote_anio, 
+                  S.delegacion AS num_del, D.descripcion AS delegacion_descripcion, 
+                  V.delegacion AS num_del_val, 
+                  S.subdelegacion AS num_subdel, SD.descripcion AS subdelegacion_descripcion, 
+                  S.nombre, S.primer_apellido, S.segundo_apellido, 
+                  S.matricula, S.curp, S.curp_correcta, S.cargo, S.usuario, 
+                  M.descripcion AS movimiento_descripcion, 
+                  G2.descripcion AS grupo_actual, G1.descripcion AS grupo_nuevo, 
+                  S.comentario, 
+                  CR.id_causarechazo AS causa_rechazo,
+                  CR.descripcion AS descripcion_causa_rechazo,
+                  RM.id_rechazomainframe as causa_rechazo_MAINFRAME,
+                  RM.descripcion as descripcion_causa_rechazo_MAINFRAME,
+                  L.fecha_atendido as fecha_atendido,
+                  RL.fecha_correo as timestamp_correo_MAINFRAME,
+                  RL.archivo as correo_MAINFRAME,
+                  RS.usuario_mainframe as usuario_MAINFRAME,
+
+CASE 
+WHEN ( S.id_causarechazo <> 0 ) THEN S.comentario
+WHEN ( S.id_causarechazo = 0 AND RS.id_rechazomainframe <> 0 ) THEN CONCAT( IF( S.comentario IS NULL, "", CONCAT( S.comentario, " " ) ), "/", RM.descripcion, IF( RS.comentario IS NULL, "", CONCAT( " (", RS.comentario, ")" ) ) ) 
+WHEN ( S.id_causarechazo = 0 AND RS.id_rechazomainframe = 0 ) THEN CONCAT ( IF( S.comentario IS NULL, "", CONCAT( S.comentario, " " ) ), IF( RS.comentario IS NULL, "", CONCAT( "(", RS.comentario, ")" ) ) )
+ELSE "OTRO"
+END AS "Observaciones",
+                  S.archivo, 
+                  CONCAT(DU.nombre, " ", DU.primer_apellido) AS creada_por,
+                  V.archivo AS archivovalija
+                FROM 
+                ( ( ( ( ( ( ( ( ( (
+                  ( ctas_solicitudes S LEFT JOIN ( ctas_resultadosolicitudes RS, ctas_rechazosmainframe RM, ctas_resultadolotes RL )
+                    ON ( ( S.id_solicitud = RS.id_solicitud AND RM.id_rechazomainframe = RS.id_rechazomainframe ) AND RS.id_resultadolote = RL.id_resultadolote ) )
+                    JOIN dspa_usuarios DU
+                        ON S.id_user = DU.id_user ) 
+                      JOIN `ctas_valijas` V
+                        ON S.id_valija = V.id_valija )
+                          JOIN `ctas_lotes` L
+                            ON S.id_lote = L.id_lote )
+                              JOIN ctas_movimientos M
+                                ON S.id_movimiento = M.id_movimiento )
+                                  JOIN ctas_grupos G1
+                                    ON S.id_grupo_nuevo = G1.id_grupo )
+                                      JOIN ctas_grupos G2
+                                        ON S.id_grupo_actual = G2.id_grupo )
+                                          JOIN dspa_delegaciones D
+                                            ON S.delegacion = D.delegacion ) 
+                                              JOIN dspa_subdelegaciones SD
+                                                ON ( S.subdelegacion = SD.subdelegacion AND D.delegacion = SD.delegacion ) )
+                                                  JOIN dspa_delegaciones D2
+                                                    ON V.delegacion = D2.delegacion )
+                                                      JOIN ctas_causasrechazo CR
+                                                        ON S.id_causarechazo = CR.id_causarechazo )
+                WHERE   S.usuario LIKE "%' . $usuario . '%" ';
+
+        $query = $query . " ORDER BY S.fecha_captura_ca DESC, S.id_solicitud ASC, S.fecha_modificacion DESC;";
           
         /*echo( $query );*/
         $data = mysqli_query($dbc, $query);
@@ -130,8 +161,10 @@
         /*echo '<th>Matrícula</th>';*/
         echo '<th>Usuario(Mov)</th>';
         echo '<th>Grupo Actual->Nuevo</th>';
+        echo '<th>Causa Rechazo DSPA</th>';
+        echo '<th>Causa Rechazo Mainframe</th>';
         echo '<th>Comentario</th>';
-        echo '<th>Causa Rechazo</th>';
+        echo '<th>Fecha Atención Mainframe</th>';
         echo '<th>PDF</th>';
         echo '</tr>';
 
@@ -154,10 +187,7 @@
           else {
             echo '<td>(Sin PDF aún)</a></td>';
           }
-          /*echo '<td><a href="vervalija.php?id_valija=' . $row['id_valija'] . '">' . $row['id_valija'] . '</a></td>';*/
 
-
-/*          echo '<td>' . $row['num_oficio_ca'] . '</td>';*/
           echo '<td>' . $row['fecha_captura_ca'] . '</td>';
           echo '<td>' . $row['creada_por'] . '</td>';
           echo '<td class="mensaje">' . $row['num_del_val'] . ' (' . $row['num_del'] . ')' . $row['delegacion_descripcion'] . ' - (' . $row['num_subdel'] . ')' . $row['subdelegacion_descripcion'] . '</td>';
@@ -165,10 +195,46 @@
           /*echo '<td>' . $row['matricula'] . '</td>'; */
           echo '<td class="mensaje"><a target="_blank" alt="Ver/Editar" href="versolicitud.php?id_solicitud=' . $row['id_solicitud'] . '">' . $row['usuario'] . ' (' . $row['movimiento_descripcion'] . ')</a></td>';
           echo '<td>' . $row['grupo_actual'] . '>' . $row['grupo_nuevo'] . '</td>'; 
+          
+          switch ( $row['causa_rechazo'] ) {
+            case 0:
+              echo '<td>' . $row['causa_rechazo'] .'-' . $row['descripcion_causa_rechazo'] . '</td>';
+              if ( is_null( $row['fecha_atendido'] ) && is_null( $row['causa_rechazo_MAINFRAME'] ) )
+                echo '<td>EN ESPERA RESPUESTA MAINFRAME</td>';
+              elseif ( !is_null( $row['fecha_atendido'] ) && is_null( $row['causa_rechazo_MAINFRAME'] ) )
+                echo '<td>SIN DOCUMENTAR AÚN RESPUESTA MAINFRAME</td>';
+              else
+                switch ( $row['causa_rechazo_MAINFRAME'] ) {
+                  case 0:
+                    echo '<td class="mensaje" align=center>ATENDIDA (' . $row['usuario_MAINFRAME'] . ')</td>';
+                    break;
+                  default:
+                    # code...
+                    echo '<td class="error">' . $row['causa_rechazo_MAINFRAME'] .'-' . $row['descripcion_causa_rechazo_MAINFRAME'] . '</td>';
+                    break;
+                }
+              break;
+
+            default:
+              # code...
+              echo '<td class="error">' . $row['causa_rechazo'] .'-' . $row['descripcion_causa_rechazo'] . '</td>';
+              echo '<td class="error">SIN TRAMITAR A MAINFRAME-SIN DOCUMENTAR</td>';
+              break;
+          }
+
           echo '<td>' . $row['comentario'] . '</td>';
-          if ( !empty( $row['id_causarechazo'] ) && $row['id_causarechazo'] <> 0 )
-            echo '<td class="error">' . $row['id_causarechazo'] .'-' . $row['causa_rechazo'] . '</td>';
-          else echo '<td>' . $row['id_causarechazo'] .'-' . $row['causa_rechazo'] . '</td>';
+
+          /*echo '<td>' . $row['timestamp_correo_MAINFRAME'] . '</td>';
+
+          correo_MAINFRAME*/
+          if (!empty($row['correo_MAINFRAME'])) {
+            echo '<td><a href="' . MM_UPLOADPATH_MSG . '\\' . $row['correo_MAINFRAME'] . '"  target="_new">' . $row['timestamp_correo_MAINFRAME'] . '</a></td>';
+          }
+          else {
+            echo '<td>(PDF aún no disponible)</a></td>';
+            /*echo '<td>' . $row['timestamp_correo_MAINFRAME'] . '</td>';*/
+          }
+
           if (!empty($row['archivo'])) {
             echo '<td><a href="' . MM_UPLOADPATH_CTASSINDO . '\\' . $row['archivo'] . '"  target="_new">Ver PDF</a></td>';
           }
