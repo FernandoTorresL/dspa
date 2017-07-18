993a7420f3efd4527742a1050783240b5b303e4d
Fernando Torres <fertorresmx@gmail.com>
2017-07-18 18:04:25 -0500

    Cambio de archivo de conexion a BD

--- agregarlote.php
+++ agregarlote.php
@@ -2,7 +2,7 @@
     require_once('commonfiles/startsession.php');
 
     require_once('lib/ctas_appvars.php');
-    require_once('lib/connectvars.php');
+    require_once('lib/connectBD.php');
 
     require_once('commonfiles/funciones.php');
 
